443f294cc3844022e69be1fa053b17f6ed4d538e
danielledoesburg <daniellemd@gmail.com>
2024-02-01 15:00:56 +0100

    Install Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Recipe routes
-Route::prefix('recipes')->controller(RecipeController::class)->group(function() {
-    Route::get('/', 'index');
-    Route::get('/{id}', 'show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
